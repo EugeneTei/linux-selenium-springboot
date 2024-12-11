46e527bd8c900fbaaf4d71bebdadc1928fa9f3de
TW - IT - Eugene Zheng <113669967+EugeneTei@users.noreply.github.com>
2024-12-11 11:39:16 +0800

    first commit

---
+++ src/main/java/com/shoalter/LinuxSeleniumSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.shoalter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxSeleniumSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxSeleniumSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoalter/LinuxSeleniumSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoalter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxSeleniumSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
